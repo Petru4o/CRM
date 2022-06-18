3342bf42e8380e313d74869c144dc7b9f8809d0e
Petru4o <saliypetro22@gmail.com>
2022-06-19 02:05:51 +0300

    Creating Account settings page

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import Group
 
 from .decorators import unauthenticated_user, allowed_users, admin_only
 from .models import Product, Customer, Order
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filter import OrderFilter
 
 
@@ -97,6 +97,21 @@ def userPage(request):
         'pending': pending
     }
     return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'accounts/account_settings.html', context)
 
 
 @login_required(login_url='login')
